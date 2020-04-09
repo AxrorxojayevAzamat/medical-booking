391db465ad10bc12a6356c53ed2d9f033bd546f2
Jamoliddin <j.sattarov@uztelocom.uz>
2020-04-09 11:36:07 +0500

    added admin lte template

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,11 +81,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Med Booking
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
+                    <a href="{{ url('test') }}">Test</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://blog.laravel.com">Blog</a>
