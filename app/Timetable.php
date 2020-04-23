83b568bd2c8636bab243b5ca9993c9840e9e74a6
e.fomichov <monstermost@yandex.ru>
2020-04-23 15:03:29 +0500

    fixed datatable bugs

---
+++ app/Timetable.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Time extends Model
+{
+    protected $primaryKey = 'id';
+    protected $fillable = ['scheduleType',
+        'interval',
+        'monday_start',
+        'monday_end',
+        'tuesday_start',
+        'tuesday_end',
+        'wednesday_start',
+        'wednesday_end',
+        'thursday_start',
+        'thursday_end',
+        'friday_start',
+        'friday_end',
+        'saturday_start',
+        'saturday_end',
+        'sunday_start',
+        'sunday_end',
+        'odd_start',
+        'odd_end',
+        'even_start',
+        'even_end',
+        'day_off_start',
+        'day_off_end',
+        'created_by',
+        'updated_by',];
+    }
