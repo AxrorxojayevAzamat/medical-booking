9cad4f62e4e3427f0ccca6c6b5042cd929b6ae5b
Jamoliddin <j.sattarov@uztelocom.uz>
2020-05-25 23:37:03 +0500

    changes

--- public/js/custom.js
+++ public/js/custom.js
@@ -12,9 +12,9 @@ $(function () {
     });
 
     //Datemask dd/mm/yyyy
-    $('#datemask').inputmask('dd/mm/yyyy', { 'placeholder': 'dd/mm/yyyy' })
+    $('#datemask').inputmask('dd/mm/yyyy', {'placeholder': 'dd/mm/yyyy'})
     //Datemask2 mm/dd/yyyy
-    $('#datemask2').inputmask('mm/dd/yyyy', { 'placeholder': 'mm/dd/yyyy' })
+    $('#datemask2').inputmask('mm/dd/yyyy', {'placeholder': 'mm/dd/yyyy'})
     //Money Euro
     $('[data-mask]').inputmask()
 
@@ -30,21 +30,21 @@ $(function () {
     })
     //Date range as a button
     $('#daterange-btn').daterangepicker(
-        {
-            ranges: {
-                'Today': [moment(), moment()],
-                'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
-                'Last 7 Days': [moment().subtract(6, 'days'), moment()],
-                'Last 30 Days': [moment().subtract(29, 'days'), moment()],
-                'This Month': [moment().startOf('month'), moment().endOf('month')],
-                'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+            {
+                ranges: {
+                    'Today': [moment(), moment()],
+                    'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+                    'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+                    'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+                    'This Month': [moment().startOf('month'), moment().endOf('month')],
+                    'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+                },
+                startDate: moment().subtract(29, 'days'),
+                endDate: moment()
             },
-            startDate: moment().subtract(29, 'days'),
-            endDate: moment()
-        },
-        function (start, end) {
-            $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
-        }
+            function (start, end) {
+                $('#reportrange span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'))
+            }
     )
 
     //Timepicker
@@ -56,24 +56,32 @@ $(function () {
     //Bootstrap Duallistbox
     $('.duallistbox').bootstrapDualListbox()
 
+    //Colorpicker
+    $('.my-colorpicker1').colorpicker()
+    //color picker with addon
+    $('.my-colorpicker2').colorpicker()
+
+    $('.my-colorpicker2').on('colorpickerChange', function (event) {
+        $('.my-colorpicker2 .fa-square').css('color', event.color.toString());
+    });
+
     $("input[data-bootstrap-switch]").each(function () {
         $(this).bootstrapSwitch('state', $(this).prop('checked'));
     });
 
 })
 
-//adding region
+//////////// for adding region
 $(document).ready(function () {
     $('select[name="reg"]').on('change', function () {
         let region_id = $(this).val();
         if (region_id) {
             $.ajax({
-                url: 'findCity/' + region_id,
+                url: '/region/findCity/' + region_id,
                 type: 'GET',
                 dataType: 'json',
 
                 success: function (data) {
-                    console.log(data);
                     $('select[name="region"]').empty();
                     $.each(data, function (key, value) {
                         $('select[name="region"]').append('<option value="' + key + '">' + value + '</option>');
@@ -85,45 +93,42 @@ $(document).ready(function () {
         }
     });
 });
+//////////// for adding region
 
 //////////// for adminCallCenter findDoctorByCity
 $(document).ready(function () {
     $('#region').on('change', function () {
         let region_id = $(this).val();
-        if (region_id) {
-            $.ajax({
-                url: 'callcenter/findDoctor/?',
-                type: 'GET',
-                data: {
-                    region_id: region_id
-                },
-                dataType: 'json',
+        $.ajax({
+            url: 'callcenter/findDoctorByRegion/?',
+            type: 'GET',
+            data: {
+                region: region_id
+            },
+            dataType: 'json',
 
-                success: function (data) {
-                    $('#city').empty();
-//                    $('select[name="city"]').append('<option selected></option>');
-                    $.each(data, function (key, value) {
-                        $('#city').append('<option value="' + key + '">' + value + '</option>');
-                    });
-                }
-            });
-        } else {
-            $('city').empty();
-        }
+            success: function (data) {
+                $('#city').empty();
+                $('#city').append('<option></option>');
+                $.each(data, function (key, value) {
+                    $('#city').append('<option value="' + key + '">' + value + '</option>');
+                });
+            }
+        });
     });
 });
 
 //////////// for adminCallCenter findDoctor
 $(document).ready(function () {
-    $('#type').on('change', function () {
-        let type_id = $(this).val();
+    $('#city').on('change', function () {
+        let type_id = $('#type').val();
         let city_id = $('#city').val();
         $.ajax({
-            url: 'callcenter/findDoctor/?',
+            url: 'callcenter/findDoctorByType/?',
             type: 'GET',
             data: {
-                type_id: type_id,
-                city_id: city_id
+                type: type_id,
+                city: city_id
             },
             dataType: 'json',
 
@@ -138,13 +143,34 @@ $(document).ready(function () {
 
     });
 });
-<<<<<<< HEAD
 
-// validation time
-=======
+//////////// for adminCallCenter findDoctor
+$(document).ready(function () {
+    $('#type').on('change', function () {
+        let type_id = $(this).val();
+        let city_id = $('#city').val();
+        $.ajax({
+            url: 'callcenter/findDoctorByType/?',
+            type: 'GET',
+            data: {
+                type: type_id,
+                city: city_id
+            },
+            dataType: 'json',
+
+            success: function (data) {
+                $('#clinic').empty();
+                $('#clinic').append('<option></option>');
+                $.each(data, function (key, value) {
+                    $('#clinic').append('<option value="' + key + '">' + value + '</option>');
+                });
+            }
+        });
+
+    });
+});
 ///////////  for validation time
 //Time  range picker
->>>>>>> f39e9ea90df6ce7c23d000e6e9d8daa5f97501fb
 $('#timepickerstart').datetimepicker({
     use24hours: true,
     format: 'HH:mm'
@@ -154,6 +180,20 @@ $('#timepickerend').datetimepicker({
     use24hours: true,
     format: 'HH:mm'
 })
+
+///////////  for validation time
+//Time  range picker
+$('#timepickerstart').datetimepicker({
+    use24hours: true,
+    format: 'HH:mm'
+})
+
+$('#timepickerend').datetimepicker({
+    use24hours: true,
+    format: 'HH:mm'
+})
+
+///////////  for validation time
 
 
 // $(document).ready(function() {
