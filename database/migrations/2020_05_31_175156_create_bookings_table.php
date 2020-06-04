39e94b144a72cba48f588a52ffc2d2cb9b2ff80c
x.shuhrat <shuhrat78jon@gmail.com>
2020-06-04 09:53:55 +0500

    fix migrations

---
+++ database/migrations/2020_05_31_175156_create_bookings_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookingsTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('doctor_id');
+            $table->unsignedInteger('clinic_id');
+            $table->date('booking_date')->nullable();
+            $table->time('time_start')->nullable();
+            $table->time('time_finish')->nullable();
+            $table->text('description')->nullable();
+            $table->tinyInteger('status')->nullable();
+
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('restrict');
+            $table->foreign('doctor_id')->references('id')->on('users')->onDelete('restrict');
+            $table->foreign('clinic_id')->references('id')->on('clinics')->onDelete('restrict');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('bookings');
+    }
+
+}
