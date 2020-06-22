8ee0e5ec4465c8dddf9ae42b6e5c40cb91c8bf28
kodirov257 <abdurahmonkodirov97@gmail.com>
2020-06-22 19:29:50 +0500

    fixed register controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -4,93 +4,71 @@ namespace App\Http\Controllers\Auth;
 
 use App\Entity\User\User;
 use App\Http\Controllers\Controller;
-use App\Entity\User\Role;
-use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
-class RegisterController extends Controller // TODO: fix it
+class RegisterController extends Controller
 {
-    /*
-      |--------------------------------------------------------------------------
-      | Register Controller
-      |--------------------------------------------------------------------------
-      |
-      | This controller handles the registration of new users as well as their
-      | validation and creation. By default this controller uses a trait to
-      | provide this functionality without requiring any additional code.
-      |
-     */
+    use RegistersUsers;
 
-use RegistersUsers;
-
-    /**
-     * Where to redirect users after registration.
-     *
-     * @var string
-     */
     //protected $redirectTo = '/home';
     protected $redirectTo;
 
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct() {
-        if (Auth::check() && Auth::user()->role->id == 1)
+    public function __construct()
+    {
+        if (Auth::check() && Auth::user()->isAdmin())
         {
             $this->redirectTo = route('admin.home');
-
-        }else{
-
+        } else {
             $this->redirectTo =  route('patient.dashboard1');
         }
 
         $this->middleware('guest');
     }
 
-    /**
-     * Get a validator for an incoming registration request.
-     *
-     * @param  array  $data
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
     protected function validator(array $data) {
         return Validator::make($data, [
-                    'name' => ['required', 'string', 'max:255'],
-                    'lastname' => ['required', 'string', 'max:255'],
-                    'patronymic' => ['required', 'string', 'max:255'],
-                    'phone' => ['required', 'string', 'max:18', 'unique:users'],
-                    'birth_date' => ['required', 'date'],
-                    'gender' => ['required', 'integer', 'min:0', 'max:1'],
-                    'email' => ['required', 'string', 'email', 'max:255', 'unique:users', 'confirmed'],
-                    'password' => ['required', 'string', 'min:2', 'confirmed'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users', 'confirmed'],
+            'phone' => ['required', 'string', 'max:18', 'unique:users'],
+            'password' => ['required', 'string', 'min:2', 'confirmed'],
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'middle_name' => ['required', 'string', 'max:255'],
+            'birth_date' => ['required', 'date'],
+            'gender' => ['required', 'integer', 'min:0', 'max:1'],
         ]);
     }
 
-    /**
-     * Create a new user instance after a valid registration.
-     *
-     * @param  array  $data
-     * @return \App\Entity\User\User
-     */
-    protected function create(array $data) {
-        $role = Role::where('slug', 'user')->first();
-        $user = User::create([
-                    'name' => $data['name'],
-                    'lastname' => $data['lastname'],
-                    'patronymic' => $data['patronymic'],
-                    'phone' => $data['phone'],
-                    'birth_date' => $data['birth_date'],
-                    'gender' => $data['gender'],
-                    'email' => $data['email'],
-                    'password' => Hash::make($data['password']),
-                    'status' => User::STATUS_ACTIVE,
-                    'role' => $role->id,
-        ]);
+    protected function create(array $data)
+    {
+        DB::beginTransaction();
+        try {
+            $user = User::create([
+                'email' => $data['email'],
+                'phone' => $data['phone'],
+                'password' => bcrypt($data['password']),
+                'status' => User::STATUS_ACTIVE,
+                'role' => User::ROLE_USER,
+            ]);
 
-        return $user;
+            $profile = $user->profile()->create([
+                'first_name' => $data['first_name'],
+                'last_name' => $data['last_name'],
+                'patronymic' => $data['middle_name'],
+                'birth_date' => $data['birth_date'],
+                'gender' => $data['gender'],
+            ]);
+
+            DB::commit();
+
+            return $user;
+        } catch (\Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+
     }
 
 }
