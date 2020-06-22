2985432ff64d3d5b20a82c9b232626317f2f995a
kodirov257 <abdurahmonkodirov97@gmail.com>
2020-06-22 20:40:05 +0500

    fixes

--- routes/web.php
+++ routes/web.php
@@ -21,45 +21,37 @@ Route::group(['as' => 'admin.', 'prefix' => 'admin',  'namespace' => 'Admin', 'm
     Route::resource('clinic', 'ClinicController');
     Route::resource('celebration', 'CelebrationController');
 
-    Route::group(
-        [ 'prefix' => 'users', 'as' => 'users.' ],
-        function () {
-            Route::post('{user}/store-specializations', 'UserController@storeSpecializations')->name('store-specializations');
-            Route::get('{user}/specializations', 'UserController@specializations')->name('specializations');
+    Route::group([ 'prefix' => 'users', 'as' => 'users.' ], function () {
+        Route::post('{user}/store-specializations', 'UserController@storeSpecializations')->name('store-specializations');
+        Route::get('{user}/specializations', 'UserController@specializations')->name('specializations');
 
-            Route::post('{user}/store-clinics', 'UserController@storeClinics')->name('store-clinics');
-            Route::get('{user}/user-clinics', 'UserController@userClinics')->name('user-clinics');
+        Route::post('{user}/store-clinics', 'UserController@storeClinics')->name('store-clinics');
+        Route::get('{user}/user-clinics', 'UserController@userClinics')->name('user-clinics');
         }
     );
 
-    Route::group(
-        ['prefix' => 'region', 'as' => 'region.'],
-        function () {
-            Route::get('/', 'RegionController@index')->name('index');
+    Route::group(['prefix' => 'region', 'as' => 'region.'], function () {
+        Route::get('/', 'RegionController@index')->name('index');
 
-            Route::get('create', 'RegionController@create')->name('create');
-            Route::get('createCity', 'RegionController@createCity')->name('createCity');
-            Route::get('createDistrict', 'RegionController@createDistrict')->name('createDistrict');
-            Route::get('findCity/{id}', 'RegionController@findCity');
+        Route::get('create', 'RegionController@create')->name('create');
+        Route::get('createCity', 'RegionController@createCity')->name('createCity');
+        Route::get('createDistrict', 'RegionController@createDistrict')->name('createDistrict');
+        Route::get('findCity/{id}', 'RegionController@findCity');
 
-            Route::get('edit/{region}', 'RegionController@edit')->name('edit');
-            Route::get('editCity/{region}', 'RegionController@editCity')->name('editCity');
-            Route::get('editDistrict/{region}', 'RegionController@editDistrict')->name('editDistrict');
+        Route::get('edit/{region}', 'RegionController@edit')->name('edit');
+        Route::get('editCity/{region}', 'RegionController@editCity')->name('editCity');
+        Route::get('editDistrict/{region}', 'RegionController@editDistrict')->name('editDistrict');
 
-            Route::post('/', 'RegionController@store')->name('store');
-            Route::patch('show/{region}', 'RegionController@update')->name('update');
-            Route::delete('{region}', 'RegionController@destroy')->name('destroy');
-        }
-    );
+        Route::post('/', 'RegionController@store')->name('store');
+        Route::patch('show/{region}', 'RegionController@update')->name('update');
+        Route::delete('{region}', 'RegionController@destroy')->name('destroy');
+    });
 
 
-    Route::group(
-        ['prefix' => 'clinics/{clinic}','as' => 'clinic.'],
-        function () {
-            Route::get('main-photo', 'ClinicController@mainPhoto')->name('main-photo');
-            Route::get('add-main-photo', 'ClinicController@addMainPhoto')->name('add-main-photo');
-        }
-    );
+    Route::group(['prefix' => 'clinics/{clinic}','as' => 'clinic.'], function () {
+        Route::get('main-photo', 'ClinicController@mainPhoto')->name('main-photo');
+        Route::get('add-main-photo', 'ClinicController@addMainPhoto')->name('add-main-photo');
+    });
 
     // Route::group(
     //     ['prefix' => 'timetables/','as' => 'timetables.'],
@@ -77,27 +69,23 @@ Route::group(['as' => 'admin.', 'prefix' => 'admin',  'namespace' => 'Admin', 'm
 
     Route::delete('/timetable/delete{id}', 'TimeTableController@destroy')->name('timetables.destroy');
 
-    Route::group(
-        ['prefix' => 'call-center', 'as' => 'call-center.'],
-        function () {
-            Route::get('/', 'CallCenter\CallCenterController@index')->name('index');
-            Route::get('/findCity1/{id}', 'CallCenter\CallCenterController@findCity1');
+    Route::group(['prefix' => 'call-center', 'as' => 'call-center.'], function () {
+        Route::get('/', 'CallCenter\CallCenterController@index')->name('index');
+        Route::get('/findCity1/{id}', 'CallCenter\CallCenterController@findCity1');
 
-            Route::get('/findDoctor', 'CallCenter\CallCenterController@findDoctor');
+        Route::get('/findDoctor', 'CallCenter\CallCenterController@findDoctor');
 
-            Route::get('/findDoctorByRegion', 'CallCenter\CallCenterController@findDoctorByRegion');
-            Route::get('/findDoctorByType', 'CallCenter\CallCenterController@findDoctorByType');
+        Route::get('/findDoctorByRegion', 'CallCenter\CallCenterController@findDoctorByRegion');
+        Route::get('/findDoctorByType', 'CallCenter\CallCenterController@findDoctorByType');
 
-            Route::get('/booking/{user}/{clinic}', 'CallCenter\CallCenterController@booking')->name('booking');
-            Route::post('/booking/', 'CallCenter\CallCenterController@bookingDoctor')->name('bookingDoctor');
+        Route::get('/booking/{user}/{clinic}', 'CallCenter\CallCenterController@booking')->name('booking');
+        Route::post('/booking/', 'CallCenter\CallCenterController@bookingDoctor')->name('bookingDoctor');
 
-            Route::get('/booking-time/{user}/{clinic}', 'CallCenter\CallCenterController@bookingTime')->name('booking-time');
-        }
-    );
+        Route::get('/booking-time/{user}/{clinic}', 'CallCenter\CallCenterController@bookingTime')->name('booking-time');
+    });
 });
 
-Route::group([ 'as' => 'patient.','prefix'=> 'patient', 'namespace'=> 'Patient', 'middleware'=>['auth', 'patient']], function (){
-
+Route::group([ 'as' => 'patient.','prefix'=> 'patient', 'namespace'=> 'Patient', 'middleware' => ['auth', 'patient']], function (){
     Route::get('', 'DashboardController@index')->name('dashboard1');
 
 });
