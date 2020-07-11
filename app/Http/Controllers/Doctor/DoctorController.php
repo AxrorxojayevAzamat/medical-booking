88faaf9d47a7f4370b658e85c5c0072b5e980f90
KhurshidYakubov <khurshid.yakubov@gmail.com>
2020-07-12 01:24:40 +0500

    doctor priyomi

--- app/Http/Controllers/Doctor/DoctorController.php
+++ app/Http/Controllers/Doctor/DoctorController.php
@@ -2,15 +2,26 @@
 
 namespace App\Http\Controllers\Doctor;
 
+use App\Entity\Book\Book;
+use App\Entity\User\User;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class DoctorController extends Controller {
 
-    public function index() {
-        return view('doctor.page');
+
+    public function profileShow() {
+        $bookings = User::find(Auth::user()->id);
+        return view('doctor.profile', compact('bookings'));
     }
 
-    public function profile_show() {
-        return view('doctor.profile');
+
+    public function doctorBookings($doctor_id){
+
+        $bookings = Book::where('doctor_id', $doctor_id)->get();
+
+
+        return view('doctor.doctor_bookings', compact('bookings'));
+
     }
 }
---
+++ resources/views/doctor/doctor_bookings.blade.php
@@ -0,0 +1,37 @@
+@foreach($bookings as $booking)
+    <table>
+        <tr>
+            <th>Order ID</th>
+            <th>User ID</th>
+            <th>Doctor ID</th>
+            <th>Clinic ID</th>
+            <th>Booking date</th>
+            <th>Start time</th>
+            <th>Finish time</th>
+            <th>Description</th>
+            <th>Payment</th>
+            <th>Status</th>
+        </tr>
+
+        <tr>
+            <td>{{ $booking->id }}</td>
+            <td>{{ $booking->user_id }}</td>
+            <td>{{ $booking->doctor_id }}</td>
+            <td>{{ $booking->clinic_id }}</td>
+            <td>{{ $booking->booking_date }}</td>
+            <td>{{ $booking->time_start }}</td>
+            <td>{{ $booking->time_finish }}</td>
+            <td>{{ $booking->description }}</td>
+            <td>{{ $booking->payment_type }}</td>
+            <td>{{ $booking->status }}</td>
+        </tr>
+
+    </table>
+    <br>
+@endforeach
+<style>
+    table, th, td {
+        border: 1px solid black;
+        border-collapse: collapse;
+    }
+</style>
