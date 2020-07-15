6738d07f10c929631c68e739669d3c96438e3636
Azamat <axrorxojayevazamat@gmail.com>
2020-07-15 18:47:22 +0500

    payme change

--- resources/views/patient/payme-click.blade.php
+++ resources/views/patient/payme-click.blade.php
@@ -36,105 +36,16 @@
 </div>
 
 @section('scripts')
+@include('patient.payme-js')
+
 <script>
-    $('.payme').css('display','none');
     $('.click').css('display','none');
-    $('.sms-payme').css('display','none');
     $('.sms-click').css('display','none');
     $('.successed').css('display','none');
 
-    $('#payme-submit').hide();
     $('#click-submit').hide();
     $('input[name="booking_date"]').hide();
 
-
-// $('.choose').on("submit", function() {
-//     infoSendPaymeUz();
-// })
-$('.payme-choose').submit(function(e) {
-    e.preventDefault();
-    $.ajaxSetup({
-        headers: {
-            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
-            'XSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
-            'X-Auth': $('#payme_merchant_id').val()
-        }
-    });
-    let clientInfo = {
-        "doctor_id": '',
-        "clinic_id": '',
-        "amount": '',
-        "booking_date": '',
-        "time_start": '',
-        "description": ''
-    };
-
-    clientInfo.doctor_id = $('#doctor_id').val();
-    clientInfo.clinic_id = $('#clinic_id').val();
-    clientInfo.amount = 15000;
-    clientInfo.booking_date = '2020-07-15';
-    clientInfo.time_start = $('#time_start').val();
-    clientInfo.description = "something";
-
-    $.ajax({
-        url: '/book/paycom/create',
-        method: "POST",
-        data: clientInfo,
-        dataType: "json",
-        success: function (data) {
-            console.log("success")
-            paymeOrderId = data.data.order_id;
-            $('.choose').css('display','none');
-            $('.payme').css('display','block');
-        },
-        error: function (data) {
-            console.log(data)
-            $('.payme').css('display','none');
-            $('.choose').css('display','block');
-            document.getElementsByClassName(".error-container").innerHTML += data.responseJSON.message;
-        }
-    })
-})
-
-// $('.payme-choose').submit(function(e) {
-//     e.preventDefault();
-//     $.ajaxSetup({
-//         headers: {
-//             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
-//             'XSRF-TOKEN': $('meta[name="csrf-token"]').attr('content'),
-//             'X-Auth': $('#payme_merchant_id').val()
-//         }
-//     });
-//     let orderCreate = {};
-//     orderCreate.doctor_id = $('#doctor_id').val();
-//     orderCreate.clinic_id = $('#clinic_id').val();
-//     orderCreate.amount = 15000;
-//     orderCreate.booking_date = '2020-07-15';
-//     orderCreate.time_start = $('#time_start').val();
-//     orderCreate.description = "click order create";
-//
-//
-//     $.ajax({
-//         url: '/book/click/create',
-//         method: "POST",
-//         data: orderCreate,
-//         dataType: "json",
-//         success: function (data) {
-//             console.log("success");
-//             paymeOrderId = data.data.order_id;
-//             $('.choose').hide();
-//             $('.click-container').show();
-//         },
-//         error: function (data) {
-//             console.log(data);
-//             $('.payme').hide();
-//             $('.choose').css('display','block');
-//             $(".error-container").val(data.message);
-//         }
-//     })
-//
-//
-// });
 </script>
 
 @endsection
