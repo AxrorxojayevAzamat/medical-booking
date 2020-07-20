90346fbbb4302e5ae8eb4e53c3ed0b591c82c070
Jamoliddin <j.sattarov@uztelocom.uz>
2020-07-20 14:46:28 +0500

    partners edit

---
+++ app/Http/Requests/PartnerRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+/**
+ * @property string $name
+ * @property string $site_url
+ * @property string $sort
+ * @property string $status
+ * @property \Illuminate\Http\UploadedFile $photo
+ */
+class PartnerRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'site_url' => 'required|string|max:255',
+            'sort' => 'required|int|unique:partners',
+            'status' => 'required|int',
+            'photo' => 'image|mimes:jpg,jpeg,png',
+        ];
+    }
+}
