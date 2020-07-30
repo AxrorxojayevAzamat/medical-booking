f748f8723adad43b0408522ba414b17a84037b77
azamat <axrorxojayevazamat@gmail.com>
2020-07-30 18:14:00 +0500

    fix first click in admin

--- config/adminlte.php
+++ config/adminlte.php
@@ -304,6 +304,11 @@ return [
                     'asset' => true,
                     'location' => '/vendor/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js',
                 ],
+                [
+                    'type' => 'js',
+                    'asset' => true,
+                    'location' => '/vendor/bootstrap/js/bootstrap.bundle.js',
+                ],
             ],
         ],
         [
