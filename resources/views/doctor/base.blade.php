a51222c4c8059f31a6393fbef982f3a262359d62
azamat <axrorxojayevazamat@gmail.com>
2020-08-04 22:41:13 +0500

    conflict with links

--- resources/views/doctor/base.blade.php
+++ resources/views/doctor/base.blade.php
@@ -1,22 +1,16 @@
 <!DOCTYPE html>
 <html>
 
-    @include('layouts.head')
-    
-    <head>
-        <link href="{{asset('vendor/select2/css/select2.min.css')}}" rel="stylesheet">
-        <link href="{{asset('vendor/bootstrap-fileinput/css/fileinput.css')}}" rel="stylesheet">
-        <link href="{{asset('vendor/bootstrap-fileinput/css/fileinput-rtl.css')}}" rel="stylesheet">
-        <link href="{{asset('vendor/daterangepicker/daterangepicker.css')}}" rel="stylesheet">
-        <link href="{{asset('css/date_picker.css')}}" rel="stylesheet">
-        <link href="{{asset('css/doctor-spec.css')}}" rel="stylesheet">
-    </head>
+    @include('doctor.head')
+
 <body>
     @include('doctor.header')
 
     @yield('content')
 
     @include('doctor.footer')
+
+    @yield('js')
     
 </body>
 	
--- resources/views/doctor/footer.blade.php
+++ resources/views/doctor/footer.blade.php
@@ -36,39 +36,54 @@
     </div>
 </div>
 <!-- Bootstrap core JavaScript-->
-<script src="{{asset('js/jquery-2.2.4.min.js') }}"></script>
-<script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+{{-- <script src="{{asset('js/jquery-2.2.4.min.js') }}"></script> --}}
+{{-- <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
 <script src="{{asset('js/jquery.easing.min.js')}}"></script>
 <script src="{{asset('js/Chart.min.js')}}"></script>
-{{-- <script src="{{asset('js/jquery.dataTables.js')}}"></script> --}}
+<script src="{{asset('js/jquery.dataTables.js')}}"></script>
 <script src="{{asset('js/dataTables.bootstrap4.js')}}"></script>
 <script src="{{asset('js/jquery.selectbox-0.2.js')}}"></script>
 <script src="{{asset('js/retina-replace.min.js')}}"></script>
-<script src="{{asset('js/jquery.magnific-popup.min.js')}}"></script>
+<script src="{{asset('js/jquery.magnific-popup.min.js')}}"></script> --}}
 <!-- Custom scripts for all pages-->
-<script src="{{asset('js/admin.js') }}"></script>
+{{-- <script src="{{asset('js/admin.js') }}"></script> --}}
 <!-- Custom scripts for this page-->
-<script src="{{asset('js/dropzone.js') }}"></script>
+{{-- <script src="{{asset('js/dropzone.js') }}"></script> --}}
+<script src="{{asset('vendor/jquery/jquery.min.js')}}"></script>
+<script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('vendor/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
+
 
 <script src="{{asset('vendor/select2/js/select2.full.min.js')}}"></script>
-<script src="{{asset('js/doctor-spes.js')}}"></script>
-<script src="{{asset('js/bootstrap-datepicker.js')}}"></script>
-{{-- <script src="{{asset('js/custom.js')}}"></script> --}}
-{{-- <script src="{{asset('js/functions.js')}}"></script> --}}
-<script src="{{asset('vendor/bootstrap-fileinput/js/fileinput.min.js')}}"></script>
-<script src="{{asset('vendor/bootstrap-switch/js/bootstrap-switch.min.js')}}"></script>
-<script src="{{asset('vendor/daterangepicker/daterangepicker.js')}}"></script>
 <script src="{{asset('vendor/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js')}}"></script>
+<script src="{{asset('vendor/moment/moment.min.js')}}"></script>
+<script src="{{asset('vendor/inputmask/min/jquery.inputmask.bundle.min.js')}}"></script>
+<script src="{{asset('vendor/bs-custom-file-input/bs-custom-file-input.min.js')}}"></script>
+
 <script src="{{asset('vendor/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js')}}"></script>
-<script src="{{asset('vendor/bootstrap-colorpicker/js/bootstrap-colorpicker.min.js')}}"></script>
+
+<script src="{{asset('vendor/bootstrap-switch/js/bootstrap-switch.min.js')}}"></script>
+<script src="{{asset('js/custom.js')}}"></script>
 
 
-{{-- <script src="{{asset('vendor/bootstrap-fileinput/js/plugins/piexif.js')}}"></script>
+ 
+<script src="{{asset('vendor/daterangepicker/daterangepicker.js')}}"></script>
+
+
+<script src="{{asset('vendor/bootstrap-fileinput/js/plugins/piexif.js')}}"></script>
 <script src="{{asset('vendor/bootstrap-fileinput/js/plugins/sortable.js')}}"></script>
 <script src="{{asset('vendor/bootstrap-fileinput/js/plugins/purify.js')}}"></script>
+<script src="{{asset('vendor/bootstrap-fileinput/js/fileinput.min.js')}}"></script>
 <script src="{{asset('vendor/bootstrap-fileinput/js/locales/ru.js')}}"></script>
 <script src="{{asset('vendor/bootstrap-fileinput/themes/fas/theme.js')}}"></script>
-<script src="{{asset('vendor/bs-custom-file-input/bs-custom-file-input.min.js')}}"></script>
-<script src="{{asset('vendor/inputmask/min/jquery.inputmask.bundle.min.js')}}"></script>
-<script src="{{asset('vendor/moment/moment.min.js')}}"></script>
-<script src="{{asset('vendor/inputmask/min/jquery.inputmask.bundle.min.js')}}"></script> --}}
+<script src="{{asset('vendor/bootstrap-colorpicker/js/bootstrap-colorpicker.min.js')}}"></script>
+<script src="{{asset('js/common_scripts.min.js')}}"></script>
+<script src="{{asset('js/functions.js')}}"></script>
+
+{{-- <script src="{{asset('vendor/inputmask/min/jquery.inputmask.bundle.min.js')}}"></script> --}}
+
+{{-- <script src="{{asset('vendor/adminlte/dist/js/adminlte.min.js')}}"></script> --}}
+
+<script src="{{asset('js/doctor-spes.js')}}"></script>
+{{-- <script src="{{asset('js/bootstrap-datepicker.js')}}"></script> --}}
+
