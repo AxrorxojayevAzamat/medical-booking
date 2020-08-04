c8fb41b4a1bfa976ea2ffdce69ff5629b04d0fcc
kodirov257 <abdurahmonkodirov97@gmail.com>
2020-08-04 18:14:15 +0500

    fixed doctors/views

--- resources/views/doctors/index.blade.php
+++ resources/views/doctors/index.blade.php
@@ -2,64 +2,62 @@
 
 @section('content')
 
-<div id="page">
-    <main class="theia-exception">
-        <div id="results">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-3">
-                        <h4>{!! trans('doctors.showing_results', ['current' => $countCurrent, 'all' => $countAll]) !!}</h4>
-                    </div>
-                    <div class="col-md-3">
-                        <ul class="row">
-                            <li class="{{ Request::is('doctors*') ? 'active' : '' }}" style="margin: auto 10px auto 30px;">
-                                <a href="{{ route('doctors.index') }}">{{ trans('menu.doctors') }}</a>
-                            </li>
-                            <li class="{{ Request::is('clinics*') ? 'active' : '' }}" style="margin: auto 10px;">
-                                <a href="{{ route('clinics.index') }}">{{ trans('menu.clinics') }}</a>
+    <div id="results">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3">
+                    <h4>{!! trans('doctors.showing_results', ['current' => $countCurrent, 'all' => $countAll]) !!}</h4>
+                </div>
+                <div class="col-md-3">
+                    <ul class="row">
+                        <li class="{{ Request::is('doctors*') ? 'active' : '' }}" style="margin: auto 10px auto 30px;">
+                            <a href="{{ route('doctors.index') }}">{{ trans('menu.doctors') }}</a>
+                        </li>
+                        <li class="{{ Request::is('clinics*') ? 'active' : '' }}" style="margin: auto 10px;">
+                            <a href="{{ route('clinics.index') }}">{{ trans('menu.clinics') }}</a>
 
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="col-md-6">
-                        <div class="search_bar_list">
-                            <input type="text" class="form-control" placeholder="{{trans('doctors.search_placeholder')}}">
-                            <input type="submit" value="{{trans('adminlte.search')}}">
-                        </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-6">
+                    <div class="search_bar_list">
+                        <input type="text" class="form-control" placeholder="{{trans('doctors.search_placeholder')}}">
+                        <input type="submit" value="{{trans('adminlte.search')}}">
                     </div>
                 </div>
             </div>
         </div>
+    </div>
 
-        <div class="filters_listing">
-            <div class="container-fluid">
-                <form action="?" method="GET">
-                    <ul class="clearfix row d-flex justify-content-center">
-                        {{-- <li>
-                            <h6>Layout</h6>
-                            <div class="layout_view">
-                                <a href="grid-list.html"><i class="icon-th"></i></a>
-                                <a href="#0" class="active"><i class="icon-th-list"></i></a>
-                                <a href="list-map.html"><i class="icon-map-1"></i></a>
-                            </div>
-                        </li> --}}
-                        {{-- <li>
-                            <h6>Поиск по имени...</h6>
-                            <div class="form-group">
-                                <input class="form-control" name="full_name" type="search" placeholder="ФИО" aria-label="Search" value="{{ request('full_name') }}">
-                            </div>
-                        </li> --}}
-                        <li>
+    <div class="filters_listing">
+        <div class="container-fluid">
+            <form action="?" method="GET">
+                <ul class="clearfix row d-flex justify-content-center">
+                    {{-- <li>
+                        <h6>Layout</h6>
+                        <div class="layout_view">
+                            <a href="grid-list.html"><i class="icon-th"></i></a>
+                            <a href="#0" class="active"><i class="icon-th-list"></i></a>
+                            <a href="list-map.html"><i class="icon-map-1"></i></a>
+                        </div>
+                    </li> --}}
+                    {{-- <li>
+                        <h6>Поиск по имени...</h6>
+                        <div class="form-group">
+                            <input class="form-control" name="full_name" type="search" placeholder="ФИО" aria-label="Search" value="{{ request('full_name') }}">
+                        </div>
+                    </li> --}}
+                    <li>
                         <h6>{{trans('filter.search_clinics')}}</h6>
-                            <div class="form-group">
+                        <div class="form-group">
                             <select id="clinic_id" name="clinic">
                                 <option value=""></option>
                                 @foreach ($clinics as $value => $label)
                                     <option value="{{ $value }}"{{ $value == request('clinic') ? ' selected' : '' }}>{{ $label }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                        </li >
+                                @endforeach
+                            </select>
+                        </div>
+                    </li >
                     <li style="margin-right: 10px">
                         <h6>{{trans('filter.search_regions')}}</h6>
                         <select id="region_id" name="region">
@@ -102,25 +100,25 @@
                     </li>
                 </ul>
             </form>
-            </div>
         </div>
+    </div>
 
-        <div class="container margin_60_35">
-            <div class="row">
-                <div class="col-lg-7">
+    <div class="container margin_60_35">
+        <div class="row">
+            <div class="col-lg-7">
 
-                    @foreach($doctors as $doctorKey => $doctorValue)
+                @foreach($doctors as $doctorKey => $doctorValue)
                     <div class="strip_list wow fadeIn">
                         <figure>
                             @if($doctorValue->profile->image)
-                            <img src="{{asset($doctorValue->profile->image)}}" alt="">
+                                <img src="{{asset($doctorValue->profile->image)}}" alt="">
                             @else
-                            <img src="{{asset('/img/565x565.jpg')}}" alt="">
+                                <img src="{{asset('/img/565x565.jpg')}}" alt="">
                             @endif
 
                         </figure>
                         @foreach($doctorValue->specializations as $spec)
-                        <small>{{$spec->name}}</small>
+                            <small>{{$spec->name}}</small>
                         @endforeach
                         <h3><a href="{{ route('doctors.show',$doctorValue) }}">{{$doctorValue->profile ? $doctorValue->profile->fullName : ''}}</a></h3>
                         <p>{{$doctorValue->profile ? substr($doctorValue->profile->about, 0, 120) . ' . . . ' : ''}}</p>
@@ -146,56 +144,53 @@
 
                         <ul>
                             @if(empty($doctorValue->clinics->pluck('location')->toArray()))
-                            <li><a href="#0" onclick="initMap()" class="btn_listing"></a></li>
+                                <li><a href="#0" onclick="initMap()" class="btn_listing"></a></li>
                             @else
-                            <li><a href="#0" onclick="initMap({{$doctorValue->clinics->pluck('location')->first()}})" class="btn_listing">{{trans('doctors.view_on_map')}}</a></li>
+                                <li><a href="#0" onclick="initMap({{$doctorValue->clinics->pluck('location')->first()}})" class="btn_listing">{{trans('doctors.view_on_map')}}</a></li>
                             @endif
                             <li><a href="{{ route('doctors.show',$doctorValue) }}">{{trans('doctors.booking')}}</a></li>
                         </ul>
                     </div>
-                    
 
-                    @endforeach
-                    {{ $doctors->links() }}
 
-                </div>
-                <!-- /col -->
+                @endforeach
+                {{ $doctors->links() }}
 
-                <aside class="col-lg-5" id="sidebar">
-                    <div id="map_listing" class="normal_list"> 
-                       <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"/>
-                             <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"></script>
-                       
-                       <style>
-                           #map { height: 550px; width: 599px; }
-                       </style>
-                      </head>
-                 {{-- <body> --}}
-                       <div id="map"></div>
-                       
+            </div>
+            <!-- /col -->
+
+            <aside class="col-lg-5" id="sidebar">
+                <div id="map_listing" class="normal_list">
+                    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.6.0/dist/leaflet.css"/>
+                    <script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"></script>
+
+                    <style>
+                        #map { height: 550px; width: 599px; }
+                    </style>
+                    </head>
+                    {{-- <body> --}}
+                    <div id="map"></div>
+
                     <script>
-                            var map = L.map('map').setView([41.311081, 69.240562], 12);
-                            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar', attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'})
+                        var map = L.map('map').setView([41.311081, 69.240562], 12);
+                        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar', attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'})
                             .addTo(map);
-                         
-                L.marker([{"doctorId":53,"locations":["41.2704736,69.2134647","41.3191884,69.2382324"]},{"doctorId":42,"locations":["41.2704736,69.2134647"]},{"doctorId":39,"locations":["41.3191884,69.2382324","41.2704736,69.2134647","41.2704736,69.2134647"]},{"doctorId":4,"locations":["41.2981861,69.2120876"]},{"doctorId":44,"locations":["41.3191884,69.2382324","41.2704736,69.2134647"]}])
+
+                        L.marker([{"doctorId":53,"locations":["41.2704736,69.2134647","41.3191884,69.2382324"]},{"doctorId":42,"locations":["41.2704736,69.2134647"]},{"doctorId":39,"locations":["41.3191884,69.2382324","41.2704736,69.2134647","41.2704736,69.2134647"]},{"doctorId":4,"locations":["41.2981861,69.2120876"]},{"doctorId":44,"locations":["41.3191884,69.2382324","41.2704736,69.2134647"]}])
 
                     </script>
-                {{-- </body>  --}}
-                    </div>
-                </aside> 
-            </div>
+                    {{-- </body>  --}}
+                </div>
+            </aside>
         </div>
-    </main>
-
-</div>
+    </div>
 @endsection
 @section('scripts')
 <script>
     $('#region_id').select2();
     $('#clinic_id').select2();
     $('#specialization_id').select2({
-        width: "170px" 
+        width: "170px"
     });
 </script>
 @endsection
