f6ef792905f773d9e720890ce90423b2e2ce1152
azamat <axrorxojayevazamat@gmail.com>
2020-08-06 17:49:32 +0500

    error login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,15 +1,16 @@
-@extends('adminlte::master')
+@extends('layouts.app')
+@section('breadcrumbs', null)
 
-@section('adminlte_css_pre')
+{{-- @section('adminlte_css_pre')
 <link rel="stylesheet" href="{{ asset('vendor/icheck-bootstrap/icheck-bootstrap.min.css') }}">
-@stop
+@stop --}}
 
-@section('adminlte_css')
+{{-- @section('adminlte_css')
 @stack('css')
 @yield('css')
-@stop
+@stop --}}
 
-@section('classes_body', 'login-page')
+{{-- @section('classes_body', 'login-page') --}}
 
 @php( $login_url = View::getSection('login_url') ?? config('adminlte.login_url', 'login') )
 @php( $register_url = View::getSection('register_url') ?? config('adminlte.register_url', 'register') )
@@ -28,16 +29,16 @@
 @php( $password_reset_url = $password_reset_url ? url($password_reset_url) : '' )
 @php( $dashboard_url = $dashboard_url ? url($dashboard_url) : '' )
 @endif
-@include('layouts.header')
-@section('body')
-    <main>
+{{-- @include('layouts.header') --}}
+
+@section('content')
         <div class="bg_color_2">
 
             <div class="container margin_60_35">
                 <div id="login">
                 <h1>{{trans('auth.please_to_findoctor')}}</h1>
                     <div class="box_form">
-                        <form  action="{{ $login_url}}" method="post">
+                        <form  action="{{ route('login')}}" method="post">
                             {{ csrf_field() }}
                             <div class="form-group">
                                 <input type="email" name="email" class="form-control {{ $errors->has('email') ? 'is-invalid' : '' }}" value="{{ old('email') }}" placeholder="{{ trans('adminlte.email') }}" autofocus>
@@ -56,24 +57,23 @@
                                     </div>
                                 @endif
                             </div>
-                            <a href="{{ $password_reset_url}}" class="d-block text-center">{{ trans('adminlte.i_forgot_my_password') }}</a>
+                            {{-- <a href="{{ route('password/reset')}}" class="d-block text-center">{{ trans('adminlte.i_forgot_my_password') }}</a> --}}
                             <div class="form-group text-center add_top_20 d-flex justify-content-center">
                                 <input class="btn_1 medium" type="submit" value="{{ trans('adminlte.sign_in') }}">
                             </div>
                         </form>
                     </div>
-                    <p class="text-center link_bright"><a href="{{ $register_url }}">
+                    <p class="text-center link_bright"><a href="{{ route('register') }}">
                             {{ trans('adminlte.register_a_new_membership') }}
                         </a></p>
                 </div>
                 <!-- /login -->
             </div>
         </div>
-    </main>
 @stop
 
-@section('adminlte_js')
+{{-- @section('adminlte_js')
 <script src="{{ asset('vendor/adminlte/dist/js/adminlte.min.js') }}"></script>
 @stack('js')
 @yield('js')
-@stop
+@stop --}}
