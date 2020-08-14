fd2062e1591edfd87fff1ab0ba5b1b1e5c187784
Jamoliddin <j.sattarov@uztelocom.uz>
2020-08-14 11:15:00 +0500

    add trans to timetable

--- resources/lang/uz/timetable_edit.php
+++ resources/lang/uz/timetable_edit.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
     return [
         "week"            => "Hafta",
 
@@ -10,7 +10,7 @@
         "saturday"        => "Shanba",
         "sunday"          => "Yakshanba",
 
-        "month_even_odd"  => "Oy - just va toq kunlar",
+        "month_even_odd"  => "Oylik - juft va toq kunlar",
         "even_day"        => "Juft kunlarda ish vaqtini boshlanishi va tugashi",
         "odd_day"         => "Toq kunlarda ish vaqtini boshlanishi va tugashi",
         "interval"        => "Interval",
@@ -18,5 +18,18 @@
         "days_off_start"  => "Ta'tilning boshlanish kuni:",
         "days_off_end"    => "Ta'tilning tugash kuni:",
         "send"            => "Jo'natish",
-
+        
+        "check" => [
+            "monday"          => "Dushanba kuniga yozilgan be'morlar bor",
+            "tuesday"         => "Seshanba kuniga yozilgan be'morlar bor",
+            "wednesday"       => "Chorshanba kuniga yozilgan be'morlar bor",
+            "thursday"        => "Payshanba kuniga yozilgan be'morlar bor",
+            "friday"          => "Juma kuniga yozilgan be'morlar bor",
+            "saturday"        => "Shanba kuniga yozilgan be'morlar bor",
+            "sunday"          => "Yakshanba kuniga yozilgan be'morlar bor",
+            
+            "odd_even"          => "Toq yoki juft kunni tanlang",
+            "odd"               => "Toq kunga yozilgan be'morlar bor",
+            "even"              => "Juft kunga yozilgan be'morlar bor",
+        ],
     ];
