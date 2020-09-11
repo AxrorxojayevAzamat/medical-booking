b5222f9c73d304b63efda9b48051287a5a87d80c
x.shuhrat <shuhrat78jon@gmail.com>
2020-09-11 12:18:13 +0500

    fix store patient

--- app/Http/Requests/Admin/Users/CreatePatientRequest.php
+++ app/Http/Requests/Admin/Users/CreatePatientRequest.php
@@ -2,12 +2,11 @@
 
 namespace App\Http\Requests\Admin\Users;
 
-use App\Entity\User\User;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
 
 class CreatePatientRequest extends FormRequest
 {
+
     public function authorize()
     {
         return true;
@@ -16,13 +15,15 @@ class CreatePatientRequest extends FormRequest
     public function rules()
     {
         return [
-            'first_name' => 'required|string|max:255',
-            'last_name' => 'required|string|max:255',
-            'middle_name' => 'required|string|max:255',
-            'phone' => ['required', 'string', 'regex:/^\d{9}$/'],
-            'email' => 'required|string|email|max:255|unique:users',
+            'first_name' => ['required', 'string', 'max:255'],
+            'last_name' => ['required', 'string', 'max:255'],
+            'middle_name' => ['required', 'string', 'max:255'],
+            'phone' => ['required', 'string', 'regex:/^\d{9}$/', 'unique:users'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:2', 'confirmed'],
             'birth_date' => ['required', 'date'],
             'gender' => ['required', 'integer', 'min:0', 'max:1']
         ];
     }
+
 }
